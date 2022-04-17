c66d8c525cc03eb934ac7b8277f09ff3a92a616c
Sachin Chaubey <sachinchaubey205@gmail.com>
2022-04-17 09:35:59 +0530

    first commit

--- nested_loop_besic.java
+++ nested_loop_besic.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class nested_loop_besic{
     public static void main (String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int raw;
-        int colom;
-        String symbol = "";
+        try (Scanner scanner = new Scanner(System.in)) {
+            int raw;
+            int colom;
+            String symbol = "";
 
-        System.out.println("enter the no of row :");
-        raw = scanner.nextInt();
+            System.out.println("enter the no of row :");
+            raw = scanner.nextInt();
 
-        System.out.println("enter the no of coloum :");
-        colom = scanner.nextInt();
+            System.out.println("enter the no of coloum :");
+            colom = scanner.nextInt();
 
-        System.out.println("enter the symbol in your choice : ");
-        symbol = scanner.next();
+            System.out.println("enter the symbol in your choice : ");
+            symbol = scanner.next();
 
-        for(int i = 1 ; i <= raw ; i++){
-            System.out.println();
-            for(int j = 1 ; j <= colom; j++){
-                System.out.print(symbol + " ");
+            for(int i = 1 ; i <= raw ; i++){
+                System.out.println();
+                for(int j = 1 ; j <= colom; j++){
+                    System.out.print(symbol + " ");
+                }
             }
         }
 
--- two_d_arraylist.java
+++ two_d_arraylist.java
@@ -1,18 +1,18 @@
-import java.util.*;
+// import java.util.*;
 import java.util.ArrayList;
 
 public class two_d_arraylist{
     public static void main(String[] args){
 
-        ArrayList<ArrayList<String>> Grosary = new ArrayList();
+        ArrayList<ArrayList<String>> Grosary = new ArrayList<>();
 
-        ArrayList<String> cockList = new ArrayList();
+        ArrayList<String> cockList = new ArrayList<>();
 
         cockList.add("Cokacola");
         cockList.add("pepsi");
         cockList.add("Coffes");
 
-        ArrayList<String> fruitList = new ArrayList();
+        ArrayList<String> fruitList = new ArrayList<>();
         fruitList.add("Mangos");
         fruitList.add("banana");
         fruitList.add("orange");
