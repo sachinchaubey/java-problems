5a09273b942443588da9e9c45087e187ad870252
Sachin Chaubey <sachinchaubey205@gmail.com>
2022-04-18 22:09:36 +0530

    first commit

---
+++ function_call.java
@@ -0,0 +1,13 @@
+public class function_call{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        int c = hello(a,b);
+        System.out.println(c);
+    }
+
+    static int hello(int a,int b){
+       int z = a + b;
+       return z; 
+    }
+}
