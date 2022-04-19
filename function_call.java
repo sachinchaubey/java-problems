7c280cf4ac274382d48388514e0fee6d038e5485
Sachin Chaubey <sachinchaubey205@gmail.com>
2022-04-19 23:39:47 +0530

    first commit

--- function_call.java
+++ function_call.java
@@ -1,13 +1,13 @@
 public class function_call{
     public static void main(String[] args){
-        int a = 5;
-        int b = 6;
-        int c = hello(a,b);
+        // int a = 5;  
+        // int b = 6;   
+        int c = hello(5,6);
         System.out.println(c);
     }
 
     static int hello(int a,int b){
-       int z = a + b;
-       return z; 
+        int z = a + b;
+        return z; 
     }
 }
---
+++ std_in_out.java
@@ -0,0 +1,30 @@
+// HACKER RANK PROBLEM: stdin and stdout II (easy problem)
+
+// print given int ,double, string in form of string ,double,int in new line
+
+import java.util.Scanner;
+
+public class std_in_out{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the int no: ");
+        int a = input.nextInt();
+
+        System.out.println("Enter the Double no: ");
+        double b = input.nextDouble();
+
+        input.nextLine();
+
+
+        System.out.println("Enter the String no: ");
+        String c = input.nextLine();
+
+        // print all outputs
+        System.out.println("String: " + c);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + a);
+
+        
+    }
+}
