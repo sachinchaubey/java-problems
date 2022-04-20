ade35036f553b039830ea5faf29c868b601a6bec
Sachin Chaubey <sachinchaubey205@gmail.com>
2022-04-20 22:15:58 +0530

    first commit

---
+++ left_justify.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+// HACKER RANK : LEFT JUSTIFY PROBLEM:
+
+public class left_justify{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("=======================");
+        for(int i= 0;i<=3;i++){
+            String a = input.next();
+            int b = input.nextInt();
+            System.out.printf("%-14s %03d\n",a,b);
+        }
+        System.out.println("=======================");
+        
+    }
+}
