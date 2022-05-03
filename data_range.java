99dc026dee22e54d5dc4f6d4ca0625a383bd0370
Sachin Chaubey <sachinchaubey205@gmail.com>
2022-05-04 00:45:44 +0530

    first commit

---
+++ data_range.java
@@ -0,0 +1,7 @@
+public class data_range{
+    public static void main(String args[]){
+        System.out.println("short range " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        System.out.println("int range " + Integer.MIN_VALUE +" to "+Integer.MAX_VALUE);
+        System.out.println("long range " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+    }
+}
---
+++ data_type.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+// hacker rank data type problem
+
+public class data_type{
+    public static void main(String args[]){
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            while(n --> 0){
+                try {
+                    Long value = scanner.nextLong();
+                    System.out.println(value + " can be fitted in: ");
+                    if(value >= Short.MIN_VALUE && value <= Short.MAX_VALUE){
+                        System.out.println(" * short "  );
+                    }
+                    if( value >= Integer.MIN_VALUE && value <= Integer.MAX_VALUE){
+                        System.out.println(" * int " );
+                    }
+                    if( value >= Long.MIN_VALUE && value <= Long.MAX_VALUE){
+                        System.out.println(" * long " );
+                    }
+                    
+                }
+                catch (Exception e) {
+                    System.out.println(scanner.next() + " can't be fitted anywhere.");
+                    //TODO: handle exception
+                }
+            }
+        }
+            
+}
+
+        }
+
+    // public static void check(Long value){
+    //     System.out.println(value + " can be fitted in: ");
+    //     if(value >= Short.MIN_VALUE && value <= Short.MAX_VALUE){
+    //         System.out.println(" * short "  );
+    //     }
+    //     if( value >= Integer.MIN_VALUE && value <= Integer.MAX_VALUE){
+    //         System.out.println(" * int " );
+    //     }
+    //     if( value >= Long.MIN_VALUE && value <= Long.MAX_VALUE){
+    //         System.out.println(" * long " );
+    //     }
+    //     else if( value > Long.MAX_VALUE){
+    //         System.out.println(value + " can't be fitted anywhere ");
+    //     }
+    // }
