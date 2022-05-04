8f4a53e87e07ee4fa52c78412f8ba026034d73f0
Sachin Chaubey <sachinchaubey205@gmail.com>
2022-05-04 22:45:08 +0530

    first commit

---
+++ hacker_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class hacker_1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        while(sc.hasNext()){
+            n++;
+            System.out.println(n +" "+ sc.nextLine());
+        }
+    }
+}
