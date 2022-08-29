bbe49d5243e7ac44982b401bf7da1c71a7af2498
Sachin Chaubey <sachinchaubey205@gmail.com>
2022-08-29 17:32:37 +0530

    first commit

---
+++ mejorityElmt2.java
@@ -0,0 +1,25 @@
+public class mejorityElmt2 {
+    public static int major(int arr[]){
+        int index = 0;
+        int count = 0;
+        for(int i = 0; i<arr.length ; i++){
+            if(count == 0){
+                index = arr[i];
+                count = 1;
+            }
+            else if(index == arr[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args){
+        int arr[] = {3,2,3};
+        System.out.println(major(arr));
+
+    }
+    
+}
