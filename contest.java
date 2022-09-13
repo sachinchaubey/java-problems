bbc3d15813084239ab24ab6826c45e32700b9795
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-14 00:31:58 +0530

    first commit

---
+++ contest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// codechef problem
+
+class contest{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n != 0){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+            System.out.println(A-B);
+            System.out.println((A-B)-C);
+            n--;
+        }
+    }
+}
