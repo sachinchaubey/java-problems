02156a30eb2c77fa4a17796ed6b88228391cdbb7
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-15 21:03:57 +0530

    first commit

---
+++ pairShoes.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// codeChef problems
+
+class pairShoes{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n --> 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a <= b){
+                System.out.println(a);
+            }
+            else if(a > b){
+                int c = a -b;
+                c = c + a;
+                System.out.println(c);
+            }
+        }
+        sc.close();
+        
+    }
+}
