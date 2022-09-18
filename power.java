4129107591aeaaab4ccd3dbd368f70bcca18ac98
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-18 22:32:42 +0530

    first commit

--- power.java
+++ power.java
@@ -2,12 +2,6 @@ public class power{
     public static void main(String[] args ){
         int p;
         for(int i=0 ; i<5 ; i++){
-            // if(i == 0){
-            //     p = 1;
-            // }
-            // else{
-            //     p = (p * 2); 
-            // }
             p = (int) Math.pow(2,i);
             System.out.println(p);
         }
