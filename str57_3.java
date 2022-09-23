5051b431fadb1bb3567dafb56d82c0be37bfa0d9
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-23 12:07:10 +0530

    first commit

---
+++ str57_3.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class srt57_3{
+    /**
+    //  * @param args
+     */
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t --> 0){
+            int n = sc.nextInt();
+            String s = sc.next();
+            if(n <= 2){
+                System.out.println(s);
+            }
+            else{
+                char[] ch = s.toCharArray();
+                Arrays.sort(ch);
+                String ans = new String(ch);
+                System.out.println(ans);
+            }
+
+
+
+            
+            // System.out.println(Arrays.toString(s));
+
+        }
+        
+    }
+}
