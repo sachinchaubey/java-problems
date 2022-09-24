94bc300cee9ea7b3161a60f80ba5df6bb61fbfda
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-24 14:47:03 +0530

    first commit

---
+++ lunchtTime_3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class lunchtTime_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t --> 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int x = sc.nextInt();
+            int d= b-a;
+            x = x*2;
+            if(d % x == 0){
+                System.out.println("yes");
+            }
+            else{
+                System.out.println("no");
+            }
+        }
+    }
+}
