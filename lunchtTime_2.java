1cb958274b07dd4a2fd27e0ac9dadcfcb8f07792
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-24 14:46:47 +0530

    first commit

---
+++ lunchtTime_2.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+class lunchTime_2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t --> 0){
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int r = n-x;
+            if(x == 0 || x == n){
+                System.out.println(0);
+            }
+            else if(r > x){
+                System.out.println(x);
+            }
+            else{
+                System.out.println(r);
+            }
+        }
+    }
+}
