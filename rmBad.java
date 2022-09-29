9fb4cf5df206260285d78c2d96eb82f5853faac2
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-29 22:47:56 +0530

    first commit

---
+++ rmBad.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//codechef problem
+
+class rmBad{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t --> 0){
+            int count = 0;
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for(int i = 0; i< n ; i++){
+                a[i] = sc.nextInt();
+            }
+            for(int i = 0 ;i<n ;i++){
+                if(a[i] != a[i+1]){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
