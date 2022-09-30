34c5d682aea39152f393ae9e4da33738b1cb177a
sachinchaubey <sachinchaubey205@gmail.com>
2022-10-01 05:14:07 +0530

    first commit

---
+++ removeBad.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class removeBad{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t;
+        t =  sc.nextInt();
+        while(t-->0){
+            int n;
+            // n = sc.nextInt();
+            // Map<Interger,Integer> mp = new HashMap<>();
+
+            System.out.println("hello");
+
+            // for(int i =0;i<n;i++){
+            //     int k = sc.nextInt();
+            //     // mp.
+            // }
+
+        }
+    }
+}
