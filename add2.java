d6c6c69d80f0c64416a1b8dc7913ec3ba057779a
sachinchaubey <sachinchaubey205@gmail.com>
2022-10-02 02:21:32 +0530

    first commit

--- add2.java
+++ add2.java
@@ -1,8 +1,14 @@
 class add2{
     public static void main(String[] args){
-        int a1 = 3;
-        int a2 = 4;
-        int a3 = a1 + a2;
-        System.out.println(a3);
+        // int a1 = 3;
+        // int a2 = 4;
+        // int a3 = a1 + a2;
+        // System.out.println(a3);
+        int a[] = {2,4,5,2,3};
+        int c = 0;
+        for(int b : a){
+            c = c + b;
+        }
+        System.out.println(c);
     }
 }
