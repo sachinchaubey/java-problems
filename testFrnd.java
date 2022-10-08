a4e3af42c373dd98faaa31bed9600e2f11ab9182
sachinchaubey <sachinchaubey205@gmail.com>
2022-10-08 15:52:17 +0530

    first commit

--- testFrnd.java
+++ testFrnd.java
@@ -1,8 +1,8 @@
 // codechef pro
 import java.util.Arrays;
-import java.util.HashSet;
+// import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Arrays;
+// import java.util.Arrays;
 class testFrnd{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
