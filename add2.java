aa5aa573977c9f4c08d2a335a751a371a2050cbd
sachinchaubey <sachinchaubey205@gmail.com>
2022-10-11 01:03:45 +0530

    first commit

--- add2.java
+++ add2.java
@@ -1,14 +1,23 @@
+import java.util.ArrayList;
+// import java.util.Arrays;
+
 class add2{
     public static void main(String[] args){
         // int a1 = 3;
         // int a2 = 4;
         // int a3 = a1 + a2;
         // System.out.println(a3);
-        int a[] = {2,4,5,2,3};
-        int c = 0;
-        for(int b : a){
-            c = c + b;
+        int a[] = {2,4};
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0 ;i < a.length; i++){
+            list.add(a[i]);
         }
-        System.out.println(c);
+        System.out.println(list);
+
+        // int c = 0;
+        // for(int b : a){
+        //     c = c + b;
+        // }
+        // System.out.println(c);
     }
 }
