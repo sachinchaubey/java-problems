2fd7fd5dc77ffce66382eeaef651b71f26182a8f
sachinchaubey <sachinchaubey205@gmail.com>
2022-10-13 22:29:18 +0530

    first commit

--- podium.java
+++ podium.java
@@ -1,6 +1,6 @@
 // codechef contest
 import java.util.Scanner;
-class podium{
+class podium {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
