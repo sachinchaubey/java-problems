54c0a3496f53bccf01f593257d6c8fd2d062b172
sachinchaubey <sachinchaubey205@gmail.com>
2022-12-04 00:02:06 +0530

    first commit

--- patt.java
+++ patt.java
@@ -1,9 +1,21 @@
 public class patt {
     public static void main(String[] args){
         int n = 5;
-        for(int i = 0 ; i < n; i++){
-            // System.out.print("*" + " ");
-            for(int j = i ; j < n ; j++){
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1; j <= (n - i); j++){
+                System.out.print(" ");
+            }
+            for(int j = i ; j <= n ; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+
+        for(int i = 2; i <= n ; i++){
+            for(int j = 1; j < i ; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
                 System.out.print("* ");
             }
             System.out.println(" ");
