5b8d3327de4d37b9fcb98f2907e750249b89b230
sachinchaubey <sachinchaubey205@gmail.com>
2022-12-03 23:30:51 +0530

    first commit

---
+++ patt.java
@@ -0,0 +1,13 @@
+public class patt {
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = 0 ; i < n; i++){
+            // System.out.print("*" + " ");
+            for(int j = i ; j < n ; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        
+    }
+}
